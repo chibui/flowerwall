6bb81426a9010b2c6d290b0eb75ecaed0119254f
chi <chi_bui@outlook.com>
2016-11-07 20:01:25 +1100

    initial

---
+++ app.js
@@ -0,0 +1,70 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/loginapp'); //connecting to database
+var db = mongoose.connection;
+
+var routes = require('./routes/index');
+var users = require('.routes/users');
+
+//  Initialize app
+var app = express();
+
+//  View engine
+app.set('views', path.join(__dirname, 'views')); // assigning 'views' folder to handle views
+app.engine('handlebars', exphbs({defaultLayout:'layout'})); // set handlebars as layout engine
+app.set('view engine', 'handlebars');
+
+// BodyParser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Set static public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Express session
+app.use(session({
+    secret: 'secret', // Admin defined
+    saveUninitialized: true,
+    resave: true
+}));
+
+// Express Validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+// Connect flash
+app.use(flash());
+
+// Global Vars for flash
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash)'error_msg');
+  res.locals.error = req.flash('error');
+  next();
+})
